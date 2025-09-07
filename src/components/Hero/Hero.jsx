ed45be4150bc05e1a5255b6e3cc92115d2cde7af
BahromWeb <bahromhasanov0202@gmail.com>
2025-09-07 10:30:21 +0500

    added

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+function Hero() {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt="images" />
+        <h1>I'm Bahrom Hasanov Frontend Developer based in Uzbekistan</h1>
+        <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect with Me</div>
+            <div className="hero-resume">My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
